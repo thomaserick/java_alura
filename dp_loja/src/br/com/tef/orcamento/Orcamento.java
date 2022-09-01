592d427ecb7e277c81fe288cba5c45b38a1d0435
Thomas Erick <thomaserick.dev@gmail.com>
2022-08-31 23:52:00 -0300

    Situação do orçamento

--- dp_loja/src/br/com/tef/orcamento/Orcamento.java
+++ dp_loja/src/br/com/tef/orcamento/Orcamento.java
@@ -6,10 +6,22 @@ public class Orcamento {
 
     private BigDecimal valor;
     private int qtdeItens;
+    private String situcao;
 
     public Orcamento(BigDecimal valor, int qtdeItens) {
         this.valor = valor;
         this.qtdeItens = qtdeItens;
+    }
+
+    public void aplicarDescontoExtra() {
+        BigDecimal valorDoDescontoExtra = BigDecimal.ZERO;
+
+        if (situcao.equals("EM ANALISE")) {
+            valorDoDescontoExtra = new BigDecimal("0.05");
+        } else if (situcao.equals("APROVADO")) {
+            valorDoDescontoExtra = new BigDecimal("0.02");
+        }
+        this.valor = this.valor.subtract(valorDoDescontoExtra);
     }
 
     public BigDecimal getValor() {
