3f01895fcf6f09270bc418b7a38869d7e8bdae63
Thomas Erick <thomaserick.dev@gmail.com>
2022-09-03 13:58:10 -0300

    State

---
+++ dp_loja/src/br/com/tef/pedido/acao/EnviarEmailPedido.java
@@ -0,0 +1,11 @@
+package br.com.tef.pedido.acao;
+
+import br.com.tef.pedido.Pedido;
+
+public class EnviarEmailPedido {
+
+    public void executar(Pedido pedido) {
+        System.out.println("Enviando e-mail");
+    }
+
+}
