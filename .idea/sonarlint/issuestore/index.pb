5caa254d3e5737dbdbf9b1c4664f36418b0658b5
ThomasErick <thomas.oliveira@totvs.com.br>
2023-05-22 16:53:19 -0300

    Projeto spring aluraFood start

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+Y
+)dp_loja/src/br/com/tef/mainDescontos.java,b/b/bb6d24bd49a60ff8928e3d84047fc6b0899db644
+P
+ dp_loja/src/br/com/tef/main.java,5/6/56f5ecc0c0b6f4a4ac2e442f1927c40cc9aee075
+_
+/dp_loja/src/br/com/tef/orcamento/Orcamento.java,d/8/d86d17730503fb694292b37a978c17c86fb6c4b6
---
+++ alurafood/src/main/java/br/com/tef/alurafood/AlurafoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.tef.alurafood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlurafoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlurafoodApplication.class, args);
+	}
+
+}
